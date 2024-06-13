a2b37adb0e90965b58b551cd258aac45eff81851
unknown <cocfanlike@gmail.com>
2024-06-13 14:21:04 +0545

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import {Outlet} from 'react-router-dom'
 import Header from "./Components/Header"
+import Footer from "./Components/Footer";
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
      <Header/>
+     <Footer/>
     </>
   )
 }
