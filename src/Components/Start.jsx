d1eb94d79ad0ad0405c08fdccd39417713e94474
unknown <cocfanlike@gmail.com>
2024-06-26 22:50:37 +0545

    few css edits

--- src/Components/Start.jsx
+++ src/Components/Start.jsx
@@ -1,16 +1,66 @@
-import React from 'react'
+import React from "react";
 
 function Start() {
   return (
     <>
-    <div>Hello this Website is......</div>
-    <div> To get started do the following</div>
-    <div>Todo has much importance in our Life</div>
-    <div>Here Iam trying to learn about uploading images in huge amount using drag and drop</div>
-    <div>Future Enhancements</div>
-    <div>More Future Enhancements</div>
+      <div className="m-2 p-1 flex  flex-col space-y-5">
+        <div className="flex  p-4 bg-white text-5xl rounded-2xl  shadow-xl border-4 ">
+          Hello this Website is......
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
+          labore libero minima mollitia fugit quas ex itaque fugiat voluptas
+          aperiam veniam, vitae sint vel modi molestias non quae earum
+          aspernatur.
+        </div>
+
+        <div className="flex   p-4 bg-white text-5xl rounded-2xl  shadow-xl border-4">
+          To get started do the following
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum,
+          harum! Accusantium expedita molestias fugiat corrupti laboriosam eos
+          debitis ut, quis ex culpa provident sapiente quas amet necessitatibus
+          incidunt est velit?
+        </div>
+
+        <div className="flex  p-4 bg-white text-5xl rounded-2xl  shadow-xl border-4">
+          Todo has much importance in our Life
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
+          dolore ipsam tempore eos aperiam error exercitationem numquam
+          quibusdam eligendi cupiditate dicta, adipisci, maiores itaque.
+          Deleniti unde cumque odit perferendis eius?
+        </div>
+
+        <div className="flex  p-4 bg-white text-5xl rounded-2xl  shadow-xl border-4">
+          Here Iam trying to learn about uploading images in huge amount using
+          drag and drop
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, impedit
+          dicta saepe, blanditiis error consectetur iure dolores alias nulla
+          nihil explicabo doloribus deleniti atque nobis omnis aspernatur
+          excepturi ullam sequi!
+        </div>
+
+        <div className="flex p-4 bg-white text-5xl rounded-2xl  shadow-xl border-4">
+          Future Enhancements
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quia
+          placeat possimus dolores, modi nisi dolorem minima architecto quo
+          corporis autem necessitatibus facere sequi nostrum doloremque,
+          voluptate explicabo ipsa iste.
+        </div>
+
+        <div className="flex  p-4 bg-white text-5xl rounded-2xl  shadow-xl border-4">
+          More Future Enhancements
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
+          voluptatem. Deserunt, officiis reprehenderit ad vel unde ipsam! Ipsum
+          fuga quia vitae ipsam incidunt illo, adipisci dignissimos. Debitis
+          explicabo dolores laboriosam.
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Start
+export default Start;
