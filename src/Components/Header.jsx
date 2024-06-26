f8b17287b7da289793b282f4a62eee67ed90a322
unknown <cocfanlike@gmail.com>
2024-06-26 23:04:22 +0545

    isActive at Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -20,13 +20,13 @@ function Header() {
           <div className="flex items-center lg:order-2">
             <NavLink
               to="dnd"
-              className="text-gray-800 hover:bg-orange-200 px-4 focus:ring-4 focus:ring-red-400  rounded-lg  lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              className={({isActive}) => `${isActive ? "text-red-500" : "text-gray-800"}   lg:px-5 py-2 lg:py-2.5 mr-2 `}
             >
               Drag and Drop
             </NavLink>
             <NavLink
               to="start"
-              className="text-gray-800 hover:bg-orange-200 px-4 focus:ring-4 focus:ring-red-400 rounded-lg lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              className={({isActive}) => `${isActive ? "text-red-500" : "text-gray-800"}   lg:px-5 py-2 lg:py-2.5 mr-2 `}
             >
               Get Started
             </NavLink>
@@ -38,7 +38,9 @@ function Header() {
           >
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
-                <NavLink to="/" className="">Home</NavLink>
+                <NavLink to="/" className={({isActive}) => `${isActive ? "text-red-500" : "text-gray-800"}`}>
+                Home
+                </NavLink>
               </li>
             </ul>
           </div>
