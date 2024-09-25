725bb21aa22e82c2c4356fe170bfc31d9e11284d
unknown <cocfanlike@gmail.com>
2024-09-25 19:32:07 +0545

    Radix UI Popover

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -1,49 +1,73 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import {removeTodo}  from '../feature/todoSlice'
-
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeTodo } from "../feature/todoSlice";
+import * as Popover from "@radix-ui/react-popover";
 
 function Todos() {
-    const todos = useSelector(state => state.todos)
-    const dispatch = useDispatch()
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
   return (
     <>
-    <div className=" flex justify-center mt-5 md-3 text-3xl font-extrabold ">Your List of Todos :</div>
-    <ul className="list-none"> 
+      <div className=" flex justify-center mt-5 md-3 text-3xl font-extrabold ">
+        Your List of Todos :
+      </div>
+      <ul className="list-none">
         {todos.map((todo) => (
-            <li
-                className="mt-4 flex  justify-between items-center bg-gray-700 px-3 py-3 rounded-xl mb-8"
-                key={todo.id}
-            >
-                <div className="text-white font-serif">{todo.text}</div>
-                <button
-                    onClick ={() => dispatch(removeTodo(todo.id))}
-                    className="text-black bg-gray-300 hover:bg-red-500 rounded-2xl text-md focus:outline-none py-1 px-4"
-                >
-                   <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-                    >
-                    <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                    </svg>  
+          <li
+            className="mt-4 flex  justify-between items-center bg-gray-700 px-3 py-3 rounded-xl mb-8"
+            key={todo.id}
+          >
+            <div className="text-white font-serif">{todo.text}</div>
 
+            <Popover.Root>
+              <Popover.Trigger asChild>
+                <button
+                  //onClick={() => dispatch(removeTodo(todo.id))}
+                  className="text-black bg-gray-300 hover:bg-red-500 rounded-2xl text-md focus:outline-none py-1 px-4"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    />
+                  </svg>
                 </button>
-            </li>    
-            
-            
+              </Popover.Trigger>
+              <Popover.Portal>
+                <Popover.Content className="p-4 bg-white rounded-xl shadow-lg">
+                  <div className="text-center text-sm">
+                    Are you sure you want to delete this task?
+                  </div>
+                  <div className="flex justify-center mt-4">
+                    <button
+                      onClick={() => dispatch(removeTodo(todo.id))}
+                      className="text-black bg-gray-300 hover:bg-red-500 rounded-2xl text-md focus:outline-none py-1 px-4"
+                    >
+                      Yes
+                    </button>
+                    <Popover.Close>
+                    <button className="text-black bg-gray-300 hover:bg-gray-400 rounded-2xl text-md focus:outline-none py-1 px-4 ml-4">
+                      No
+                    </button>
+                    </Popover.Close>
+                  </div>
+                </Popover.Content>
+                {/* End of Popover.Content */}
+              </Popover.Portal>
+            </Popover.Root>
+          </li>
         ))}
-    </ul> 
-    
-    </>  
-  )
+      </ul>
+    </>
+  );
 }
 
-export default Todos
+export default Todos;
